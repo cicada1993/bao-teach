cbb09ee6a7f8e26e1cb8bba8a4ef2d06671838d0
zhuwen <zhuwen@xcuni.com>
2021-03-09 18:34:42 +0800

    添加启动方法

--- api/command.js
+++ api/command.js
@@ -1,6 +1,20 @@
+const path = require('path')
+function startServer() {
+    let nodemon = require('nodemon');
+
+    nodemon({ script: path.join(__dirname, '../server/index.js') }).on('start', function () {
+        console.log('nodemon started');
+    }).on('crash', function () {
+        console.log('script crashed for some reason');
+    });
+    // force a restart
+    nodemon.emit('restart');
+}
 
 module.exports = (req, res) => {
     console.log('收到请求', req)
+    startServer()
     const date = new Date().toString();
     res.status(200).send(date);
 }
+
